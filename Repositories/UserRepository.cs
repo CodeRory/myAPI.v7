6c61aa501ed36e576322bf4d44ba9a27c549fd1b
Marino García <marinogabriel.garcia@alight.com>
2022-10-24 17:48:16 +0200

    Finishing today

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using TodoApi.Models;
+
+namespace TodoApi.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<Employment?> CreateAsync(User newUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyCollection<Employment>> FindAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employment?> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employment?> GetByGuidAsync(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employment?> UpdateAsync(User updatedUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
